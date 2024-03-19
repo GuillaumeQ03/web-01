e2faedbfea5aa848e22503bc5cf98c93d5bf508c
GuillaumeQ <guillaumequintin2003@gmail.com>
2024-03-19 19:28:00 +0100

    Classes done

--- init/front-end/src/app/scripts/welcome.js
+++ init/front-end/src/app/scripts/welcome.js
@@ -1,39 +1,35 @@
 import template from "../views/welcome.html";
-// TODO #class: use the ES6 class keyword
-export function WelcomeComponent() {
-  // TODO #extends: call super(template)
-  this.template = template;
-}
+import { Component } from "./component";
 
-// TODO #class: turn function into a method of WelcomeComponent
-/* method WelcomeComponent.init */
-WelcomeComponent.prototype.init = function init() {
-  var form = document.querySelector("form.form-signin");
+export class WelcomeComponent extends Component {
+  constructor() {
+    super(template)
+    this.template = template;
+  }
+  init() {
+    var form = document.querySelector("form.form-signin");
+    form.addEventListener(
+      "submit",
+      // TODO #arrow-function: use arrow function instead.
+      function (event) {
+        event.preventDefault();
+        if (form.checkValidity() === false) {
+          event.stopPropagation();
+          form.classList.add("was-validated");
+        } else {
+          var name = event.srcElement.querySelector("#nickname").value;
+          var size = parseInt(event.srcElement.querySelector("#size").value);
 
-  form.addEventListener(
-    "submit",
-    // TODO #arrow-function: use arrow function instead.
-    function (event) {
-      event.preventDefault();
-      if (form.checkValidity() === false) {
-        event.stopPropagation();
-        form.classList.add("was-validated");
-      } else {
-        var name = event.srcElement.querySelector("#nickname").value;
-        var size = parseInt(event.srcElement.querySelector("#size").value);
-
-        _startGame(name, size);
-      }
-    }.bind(this),
-    false
-  );
-
-  return this;
-};
-
-// TODO #class: turn function into a method of WelcomeComponent
-function _startGame(name, size) {
-  var gamePage = "./#game";
-  // TODO #template-literals:  use template literals (backquotes)
-  window.location = gamePage + "?name=" + name + "&size=" + size;
+          this._startGame(name, size);
+        }
+      }.bind(this),
+      false
+    );
+      return this;
+  }
+  _startGame(name, size) {
+    var gamePage = "./#game";
+    // TODO #template-literals:  use template literals (backquotes)
+    window.location = gamePage + "?name=" + name + "&size=" + size;
+  }
 }
